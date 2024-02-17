01171df3a2448fe407c4357992772830b912b321
Beyza Nur Karadana <83921230+BeyzaNurKaradana@users.noreply.github.com>
2024-02-17 22:00:38 +0300

    added images

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      
+      <h1>LANDSCAPE IMAGES</h1>
+
+      <div>
+        <div className="App"></div>
+
+        <div className="imgContainer"></div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/images.js
@@ -0,0 +1,7 @@
+export default [
+    "https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg?size=626&ext=jpg&ga=GA1.1.1700460183.1708128000&semt=sph",
+    "https://images.unsplash.com/photo-1503614472-8c93d56e92ce?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8fA%3D%3D",
+    "https://cdn.pixabay.com/photo/2012/08/27/14/19/mountains-55067_1280.png",
+    "https://www.escapemotions.com/images/mainpage/images/blog_posts_bg/landing-page_blog_93303113643.jpg",
+    "https://i.natgeofe.com/n/2a832501-483e-422f-985c-0e93757b7d84/6_3x2.jpg"
+]
